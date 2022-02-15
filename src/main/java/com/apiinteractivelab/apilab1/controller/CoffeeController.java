cef0705992fd22abe725356ef2da7d63e6bdbf3f
Gurpreet1490 <g.preet08092014@gmail.com>
2022-02-15 19:33:59 +0000

    CoffeeController and test added

---
+++ src/main/java/com/apiinteractivelab/apilab1/controller/CoffeeController.java
@@ -0,0 +1,16 @@
+package com.apiinteractivelab.apilab1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class CoffeeController {
+
+    @GetMapping(value = "/coffeelover")
+    public String coffee(){
+
+        return ("I like coffee!");
+    }
+
+}
